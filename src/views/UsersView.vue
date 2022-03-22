e06dd61e2b5ecdcfa6deec7da4d0ce551baf1131
noor3030 <nnorkrka@gmail.com>
2022-03-22 23:34:38 +0300

    add user genders in edit

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -7,53 +7,10 @@
       <v-icon class="pl-2" @click="copyUrl(item)" small
         >mdi-content-copy</v-icon
       >
-      <v-icon small class="pl-2" @click="dialogEdit=true">mdi-pencil</v-icon>
+      <v-icon small class="pl-2" @click="edit(item)">mdi-pencil</v-icon>
       <v-icon small class="pl-2" @click="dialogDelete = true"
         >mdi-delete</v-icon
       >
-      <v-dialog v-model="dialogDelete" max-width="500px"
-        ><v-card>
-          <v-card-title class="text-h5"
-            >Are you sure you want to delete this item?</v-card-title
-          >
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="closeDelete"
-              >Cancel</v-btn
-            >
-
-            <v-btn color="blue darken-1" text>OK</v-btn>
-            <v-spacer></v-spacer>
-          </v-card-actions> </v-card
-      ></v-dialog>
-       <v-dialog v-model="dialogEdit" max-width="500">
-        <v-card>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    ref="name"
-                    v-model="item.name"
-                    label="Name"
-                    required
-                  >
-                  </v-text-field>
-                </v-col>
-               
-              
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="closeEdit">
-              Cancel
-            </v-btn>
-            <v-btn color="blue darken-1" text > Save </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
     </template>
 
     <template v-slot:top>
@@ -64,19 +21,92 @@
         <v-btn color="primary" class="mb-2 mr-3"> download </v-btn>
 
         <v-btn color="primary" dark class="mb-2"> New Item </v-btn>
+        <v-dialog v-model="dialogDelete" max-width="500px"
+          ><v-card>
+            <v-card-title class="text-h5"
+              >Are you sure you want to delete this item?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete"
+                >Cancel</v-btn
+              >
+
+              <v-btn color="blue darken-1" text>OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions> </v-card
+        ></v-dialog>
+        <v-dialog v-model="dialogEdit" persistent :retain-focus="false">
+          <v-card>
+            <v-card-text>
+              <v-container>
+                <v-row
+                  ><v-avatar width="100" height="100" class="pt-2">
+                    <img :src="editedItem.image" alt="alt" /> </v-avatar
+                ></v-row>
+                <v-row>
+                  <v-col>
+                    <v-text-field
+                      ref="name"
+                      v-model="editedItem.name"
+                      label="Name"
+                      required
+                    >
+                    </v-text-field>
+                  </v-col>
+                  <v-col>
+                    <v-text-field
+                      ref="en_name"
+                      v-model="editedItem.en_name"
+                      label="English Name"
+                      required
+                    >
+                    </v-text-field>
+                  </v-col>
+                  <v-col>
+                    <v-select
+                      :items="genders"
+                      v-model="editedItem.gender"
+                      label="Gender"
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <v-row
+                  ><v-col>
+                    <v-text-field
+                      ref="uot_url"
+                      v-model="editedItem.uot_url"
+                      label="Uot Url"
+                      required
+                    >
+                    </v-text-field></v-col
+                ></v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="dialogEdit = false">
+                Cancel
+              </v-btn>
+              <v-btn color="blue darken-1" text> Save </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-toolbar>
     </template>
   </v-data-table>
 </template>
 
 <script lang="ts">
-import { UsersService, Paging_User_, User } from "@/client";
+import { UsersService, Paging_User_, User, UserGender } from "@/client";
 
 export default {
   data() {
     return {
-      dialogDelete: false,
+      genders: [UserGender.MALE, UserGender.FEMALE],
+      editedItem: {} as any,
       dialogEdit: false,
+      dialogDelete: false,
       users: {} as Paging_User_,
       headers: [
         { text: "Name", value: "name" },
@@ -98,12 +128,14 @@ export default {
         `${window.location.origin}/users/${item.id}`
       );
     },
-    closeDelete(){
-      this.dialogDelete=false
+    closeDelete() {
+      this.dialogDelete = false;
     },
-    closeEdit(){
-      this.dialogEdit=false
-    }
+
+    edit(item: User) {
+      this.editedItem = item;
+      this.dialogEdit = true;
+    },
   },
   created(): void {
     this.getUsers();
